72ce6ddbd9139882971d4e96b39efa00d615384c
Ilya <vlasuyk.2015.1@mail.ru>
2022-10-13 23:59:02 +0300

    all

---
+++ 5.py
@@ -0,0 +1,12 @@
+nums = [1, 5, 2, 3, 4, 6, 1, 7]
+
+
+def get_up2(nums):
+    ups = [nums[0]]
+    for i in nums:
+        if i > max(ups):
+            ups.append(i)
+    return ups
+    
+print(get_up2(nums))
+
